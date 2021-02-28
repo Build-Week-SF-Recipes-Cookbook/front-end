b568f067431ad62a0f30464eeb7455efe8e5e1fc
MaurooRen <maurooren@gmail.com>
2021-02-27 21:16:12 -0500

    search component with styling done

--- src/Layout/Hero.js
+++ src/Layout/Hero.js
@@ -3,10 +3,17 @@ import styled from 'styled-components'
 import Search from '../components/Search';
 
 const HeroStyled = styled.section`
-    background-color: pink;
+    background-image: url("https://images.unsplash.com/photo-1513135124264-0dc58db0a576?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1049&q=80");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
     border-radius: 20px;
     display: flex;
+    flex-flow: column wrap;
+    justify-content: center; //vertical
+    align-items: flex-start; //horizontal
     margin: 2rem auto 0;
+    padding: 0 4rem;
     width: 100%;
     height: 80vh;
     max-width: 1360px;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BsSearch, BsForward } from 'react-icons/bs';
+
+const FormStyled = styled.form`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center; //vertical
+    align-items: center; //horizontal
+
+
+    background-color: #FFF;
+    border-radius: 100px;
+    box-shadow: 0 0 15px rgba(0,0,0,0.4);
+
+    span {
+        display: none;
+    }
+    label {
+        display: inline;
+        margin: 0;
+    }
+    label i {
+        border-right: 2px solid #FF8C00;
+        color: #FF8C00;
+        font-size: 1.2rem;
+        padding: 0 1.5rem;
+    }
+    label input {
+        display: inline;
+        border: none;
+        outline: none;
+        font-size: 1rem;
+        padding: 0.8rem 1.5rem;
+        width: 500px;
+    }
+    button {
+        display: inline;
+        background-color: #FF8C00;
+        border: none;
+        border-radius: 100px;
+        color: #FFF;
+        font-size: 1.5rem;
+        outline: none;
+        margin-right: 0.4rem;
+        padding: 0rem 0.4rem 0.2rem;
+    }
+    button i {
+    }
+`;
 
 const Search = () => {
     return (
-        <form>
+        <FormStyled>
             <label>
                 <span>Search</span>
+                <i><BsSearch /></i>
                 <input type="text" name="search" placeholder="What do you want to cook?" />
             </label>
-            <button>Search</button>
-        </form>
+            <button><BsForward /></button>
+        </FormStyled>
     )
 }
 
